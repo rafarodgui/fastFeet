5fbc8026645b6aecee7fe5bb35c72acc9024d73b
rafaelRodruiguesGuimaraes <rafarodgui@hotmail.com>
2020-02-10 15:04:14 -0300

    eslint, prettier e editorconfig configurados

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ const { Router } = require('express');
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ hello: 'World' })
-})
+routes.get('/', (req, res) => res.json({ hello: 'World' }));
 
 module.exports = routes;
